8b5d53b8cddd3b5ba6ddc8b2b820ba55a41df8eb
ymatuhin <ym@ymatuhin.ru>
2022-08-02 16:09:02 +0300

    Fix redirect and styles

--- src/components/redirect.tsx
+++ src/components/redirect.tsx
@@ -2,55 +2,61 @@ import { baseLocale, urls } from "../data/urls";
 import "./redirect.css";
 
 export function Redirect({ locale }: { locale: string }) {
-  const currentLocaleIndex = navigator.languages.indexOf(locale);
+  const navigatorLanguages = [
+    ...new Set(navigator.languages.map((lang) => lang.slice(0, 2))),
+  ];
+  const currentLocaleIndex = navigatorLanguages.indexOf(locale);
   const locales = Object.keys(urls);
-  const newLocale = navigator.languages.find((language) =>
+  const newLocale = navigatorLanguages.find((language) =>
     locales.includes(language),
   );
-  const newLocaleIndex = navigator.languages.indexOf(newLocale);
+  const newLocaleIndex = navigatorLanguages.indexOf(newLocale);
   if (newLocaleIndex === -1) return null;
-  if (newLocaleIndex >= currentLocaleIndex) return null;
+  if (currentLocaleIndex !== -1 && newLocaleIndex >= currentLocaleIndex)
+    return null;
 
   const languages = {
     ru: {
-      uk: "на украинском",
-      be: "на белорусском",
-      kk: "на казахском",
-      en: "на английском",
-      zh: "на китайском", // китайский
-      es: "на испанском", // испанский
-      pt: "на португальском", // Portuguese
-      fr: "на французском", // French
-      ja: "на японском", // Japanese
-      de: "на немецком", // German
-      pl: "на польском",
-      ro: "на румынском",
+      ru: "русском",
+      uk: "украинском",
+      be: "белорусском",
+      kk: "казахском",
+      en: "английском",
+      zh: "китайском", // китайский
+      es: "испанском", // испанский
+      pt: "португальском", // Portuguese
+      fr: "французском", // French
+      ja: "японском", // Japanese
+      de: "немецком", // German
+      pl: "польском",
+      ro: "румынском",
     },
     en: {
-      ru: "in Russian",
-      uk: "in Ukrainian",
-      be: "in Belarusian",
-      kk: "in Kazakh",
-      zh: "in Chinese", // китайский
-      es: "in Spanish", // испанский
-      pt: "in Portuguese", // Portuguese
-      fr: "in French", // French
-      ja: "in Japanese", // Japanese
-      de: "in German", // German
-      pl: "in Polish",
-      ro: "in Romanian",
+      en: "English",
+      ru: "Russian",
+      uk: "Ukrainian",
+      be: "Belarusian",
+      kk: "Kazakh",
+      zh: "Chinese", // китайский
+      es: "Spanish", // испанский
+      pt: "Portuguese", // Portuguese
+      fr: "French", // French
+      ja: "Japanese", // Japanese
+      de: "German", // German
+      pl: "Polish",
+      ro: "Romanian",
     },
   };
 
   return (
     <div className="redirect-panel fixed text-sm flex items-center right-2 top-2 max-w-xs rounded bg-white shadow px-5 py-3 ">
       <p class="">
-        {baseLocale === "en" && "This site is also available"}
+        {baseLocale === "en" && "This site is also available in"}
         {baseLocale === "ru" && "Этот сайт так-же доступен на"}{" "}
         {languages[baseLocale][newLocale]}
       </p>
       <a
-        class="inline-block  ml-4 px-4 py-2 bg-orange-400 rounded text-white"
+        class="inline-block flex-shrink-0 ml-4 px-4 py-2 bg-orange-400 rounded text-white"
         href={urls[newLocale]}
       >
         <span class="drop-shadow">
